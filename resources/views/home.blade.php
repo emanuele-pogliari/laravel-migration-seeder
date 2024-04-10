9f65a6dacbdab906f0441be0cb7db29461de55f9
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-10 19:03:03 +0200

    bootstrap table inserted

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,41 @@
 
 @section('content')
 
-@foreach ($trains as $train)
- 
-<ul>
-<li>{{$train->company}}</li>
-<li>{{$train->departure_station}}</li>
-<li>{{$train->arrival_station}}</li>
-<li>{{$train->departure_date}}</li>
-<li>{{$train->departure_time}}</li>
-<li>{{$train->arrival_time}}</li>
-<li>{{$train->train_code}}</li>
-<li>{{$train->number_of_coaches}}</li>
-<li>{{$train->on_time}}</li>
-<li>{{$train->cancelled}}</li>
-</ul>
-@endforeach
+<h1>Train departures</h1>
+
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Company</th>
+        <th scope="col">Departure Station</th>
+        <th scope="col">Departure Date</th>
+        <th scope="col">Departure Time</th>
+        <th scope="col">Arrival Time</th>
+        <th scope="col">Train Code</th>
+        <th scope="col">Number of Wagons</th>
+        <th scope="col">On Time</th>
+        <th scope="col">Cancelled</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($trains as $train)
+      <tr>
+        <td>{{$train->company}}</td>
+        <td>{{$train->departure_station}}</td>
+        <td>{{$train->departure_date}}</td>
+        <td>{{$train->departure_time}}</td>
+        <td>{{$train->arrival_time}}</td>
+        <td>{{$train->train_code}}</td>
+        <td>{{$train->number_of_coaches}}</td>
+        <td>{{$train->on_time}}</td>
+        <td>{{$train->cancelled}}</td>
+    </tr>
+    @endforeach
+    </tbody>
+  </table>
+
+
 
 
 @endsection
