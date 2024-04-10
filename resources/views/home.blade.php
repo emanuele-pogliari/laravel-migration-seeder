90c74817c1676d368fa524d6e5aa53266ead699c
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-10 21:31:57 +0200

    style finished

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,27 +2,26 @@
 
 @section('content')
 
-<div class="container p-4">
+<div class="container-fluid p-3 d-flex justify-content-center ">
 <div id="monitor">
-    <div id="monitorscreen">
+    <div id="monitorscreen mx-auto border border-2">
   
-<h1>Train departures</h1>
 
-<div class="row">
 
-<table class="table-striped">
+<div class="row border border-2 p-2">
+    <h1>Train departures</h1>
+<table class="table-striped ">
     <thead>
       <tr>
-        <th class="col px-3 text-center">Company</th>
-        <th class="col px-3 text-center">Departure Station</th>
-        <th class="col px-3 text-center">Arrival Station</th>
-        <th class="col px-3 text-center">Departure Date</th>
-        <th class="col px-3 text-center">Departure Time</th>
-        <th class="col px-3 text-center">Arrival Time</th>
-        <th class="col px-3 text-center">Train Code</th>
-        <th class="col px-3 text-center">Number of Wagons</th>
-        <th class="col px-3 text-center">On Time</th>
-        <th class="col px-3 text-center">Cancelled</th>
+        <th class="col px-2 py-4 text-center">Company</th>
+        <th class="col px-2 text-center">Departure Station</th>
+        <th class="col px-2 text-center">Arrival Station</th>
+        <th class="col px-2 text-center">Departure Date</th>
+        <th class="col px-2 text-center">Departure Time</th>
+        <th class="col px-2 text-center">Arrival Time</th>
+        <th class="col px-2 text-center">Train Code</th>
+        <th class="col px-2 text-center">On Time</th>
+        <th class="col px-2 text-center">Cancelled</th>
       </tr>
     </thead>
     <tbody>
@@ -35,7 +34,6 @@
         <td class="col px-3 text-center">{{$train->departure_time}}</td>
         <td class="col px-3 text-center">{{$train->arrival_time}}</td>
         <td class="col px-3 text-center">{{$train->train_code}}</td>
-        <td class="col px-3 text-center">{{$train->number_of_coaches}}</td>
         <td class="col px-3 text-center">{{$train->on_time}}</td>
         <td class="col px-3 text-center">{{$train->cancelled}}</td>
     </tr>
