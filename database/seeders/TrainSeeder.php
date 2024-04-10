da5e42e9f843cd7fd44e21528443afcf85c4cb17
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-10 16:58:30 +0200

    import faker library

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -6,24 +6,30 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 
+
+use Faker\Generator as Faker;
+
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $newTrain = new Train();
-        $newTrain->company = 'Trenitalia';
-        $newTrain->departure_station = 'Agrigento';
-        $newTrain->arrival_tation = 'Palermo';
-        $newTrain->departure_time = '11:00';
-        $newTrain->arrival_time = '14.00';
-        $newTrain->train_code = 'H6RF';
-        $newTrain->number_of_coaches  = '5';
-        $newTrain->on_time = false;
-        $newTrain->cancelled = true;
 
-        $newTrain->save();
+        for ($i = 0; $i < 8; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = 'Trenitalia';
+            $newTrain->departure_station = 'Agrigento';
+            $newTrain->arrival_tation = 'Palermo';
+            $newTrain->departure_time = '11:00';
+            $newTrain->arrival_time = '14.00';
+            $newTrain->train_code = 'H6RF';
+            $newTrain->number_of_coaches  = '5';
+            $newTrain->on_time = false;
+            $newTrain->cancelled = true;
+
+            $newTrain->save();
+        }
     }
 }
