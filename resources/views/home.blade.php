3dd5eec88c79f616ac6b2845dcef9d1844d10fee
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-11 08:52:28 +0200

    style fixed and add ternary operator for conditions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 
-<div class="container-fluid p-3 d-flex justify-content-center ">
+<div class="container-fluid p-5 d-flex justify-content-center ">
 <div id="monitor">
     <div id="monitorscreen mx-auto border border-2">
   
 
 
-<div class="row border border-2 p-2">
+<div class="row border border-2 rounded pt-3">
     <h1>Train departures</h1>
 <table class="table-striped ">
     <thead>
       <tr>
+        <th class="col px-2 text-center">Train</th>
         <th class="col px-2 py-4 text-center">Company</th>
         <th class="col px-2 text-center">Departure Station</th>
         <th class="col px-2 text-center">Arrival Station</th>
         <th class="col px-2 text-center">Departure Date</th>
         <th class="col px-2 text-center">Departure Time</th>
         <th class="col px-2 text-center">Arrival Time</th>
-        <th class="col px-2 text-center">Train Code</th>
         <th class="col px-2 text-center">On Time</th>
         <th class="col px-2 text-center">Cancelled</th>
       </tr>
@@ -27,15 +27,15 @@
     <tbody>
         @foreach ($trains as $train)
       <tr>
-        <td class="col px-3 py-2 text-center">{{$train->company}}</td>
-        <td class="col px-3 text-center">{{$train->departure_station}}</td>
-        <td class="col px-3 text-center">{{$train->arrival_station}}</td>
-        <td class="col px-3 text-center">{{$train->departure_date}}</td>
-        <td class="col px-3 text-center">{{$train->departure_time}}</td>
-        <td class="col px-3 text-center">{{$train->arrival_time}}</td>
-        <td class="col px-3 text-center">{{$train->train_code}}</td>
-        <td class="col px-3 text-center">{{$train->on_time}}</td>
-        <td class="col px-3 text-center">{{$train->cancelled}}</td>
+        <td class="col px-2 py-2 text-center">{{$train->train_code}}</td>
+        <td class="col px-2 text-center">{{$train->company}}</td>
+        <td class="col px-2 text-center">{{$train->departure_station}}</td>
+        <td class="col px-2 text-center">{{$train->arrival_station}}</td>
+        <td class="col px-2 text-center">{{$train->departure_date}}</td>
+        <td class="col px-2 text-center">{{$train->departure_time}}</td>
+        <td class="col px-2 text-center">{{$train->arrival_time}}</td>
+        <td class="col px-2 text-center">{{($train->on_time == 1) ? 'Delayed' : 'Yes'}}</td>
+        <td class="col px-2 text-center">{{$train->cancelled == 1 ? 'Yes' : ''}}</td>
     </tr>
     @endforeach
     </tbody>
